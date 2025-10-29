4d22d7a1d155e4b71ecb9bc9485349145359fca0
ElSuperDado <damian.boquete@hotmail.com>
2025-10-29 16:21:34 +0100

    Pursuing my PoC

---
+++ backend/bot.py
@@ -0,0 +1,25 @@
+import ollama
+
+def monogatari(topic: str, jlpt_n_lvl: int) -> None:
+    models = [
+        "deepseek-r1:8b",
+        "gemma3:270m",
+    ]
+
+
+    instructions: str = f"""
+                        Generate a small tale about {topic} in japanese. 
+                        This must be compiant with the JLPT N{jlpt_n_lvl} level. 
+                        Do not use kanji above N{jlpt_n_lvl} level, use hiragana instead if needed. 
+                        Your output must be in japanese only.
+                        """
+
+    stream: ollama.ChatResponse = ollama.chat(
+        model=models[0],
+        messages=[{"role": "user", "content": instructions}],
+        stream=True,
+    )
+
+
+    for chunk in stream:
+        print(chunk.message.content, end='', flush=True)
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,3 +1,32 @@
+function is_n_level_valid(n) {
+    let level = parseInt(n, 10)
 
 
-do
+    return !isNaN(level) && level > 0 && level < 6
+}
+
+function is_topic_valid(topic) {
+    return topic.length > 0
+}
+
+function print_user_settings() {
+    let topic = document.getElementById("topic-input").value
+    let n_lvl = document.getElementById("jlpt-level-input").value
+    let feedback_p = document.getElementById("feedback")
+
+    if (!is_n_level_valid(n_lvl)) {
+        feedback_p.innerHTML = `${n_lvl} is not a valid N level.`
+        feedback_p.style.color = "red"
+        return
+    }
+
+        if (!is_topic_valid(topic)) {
+        feedback_p.innerHTML = `'${topic}' is not a valid Topic.`
+        feedback_p.style.color = "red"
+        return
+    }
+    
+    
+    feedback_p.innerHTML = `Generating N${n_lvl} story about '${topic}'...`
+    feedback_p.style.color = "black"
+} 
